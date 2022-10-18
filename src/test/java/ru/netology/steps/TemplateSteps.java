35bec4ad57ff4a3ffcf056a7eaed07be20f1219c
MaksimNosov <m.s.nosov@gmail.com>
2022-10-19 01:03:24 +0300

    Task 2

--- src/test/java/ru/netology/steps/TemplateSteps.java
+++ src/test/java/ru/netology/steps/TemplateSteps.java
@@ -1,9 +1,6 @@
 package ru.netology.steps;
 
-import io.cucumber.java.ru.И;
-import io.cucumber.java.ru.Когда;
-import io.cucumber.java.ru.Пусть;
-import io.cucumber.java.ru.Тогда;
+import io.cucumber.java.ru.*;
 import ru.netology.data.DataHelper;
 import ru.netology.pages.DashboardPage;
 import ru.netology.pages.LoginPageV1;
@@ -16,24 +13,21 @@ public class TemplateSteps {
     private static DashboardPage dashboardPage;
     private static VerificationPage verificationPage;
 
-    @Пусть("открыта страница с формой авторизации {string}")
-    public void openAuthPage(String url) {
-        loginPage = open(url, LoginPageV1.class);
-    }
-
-    @Когда("пользователь пытается авторизоваться с именем {string} и паролем {string}")
-    public void loginWithNameAndPassword(String login, String password) {
-        verificationPage = loginPage.validLogin("vasya", "qwerty123");
-    }
-
-    @И("пользователь вводит проверочный код 'из смс' {string}")
-    public void setValidCode(String verificationCode) {
-        dashboardPage = verificationPage.validVerify(verificationCode);
-    }
-
-    @Тогда("происходит успешная авторизация и пользователь попадает на страницу 'Личный кабинет'")
-    public void verifyDashBoardPage() {
+    @Дано("пользователь залогинен с именем {string} и паролем {string}")
+    public void userLogged(String login, String password) {
+        loginPage = open("http://localhost:9999/", LoginPageV1.class);
+        verificationPage = loginPage.validLogin(login, password);
+        dashboardPage = verificationPage.validVerify("12345");
         dashboardPage.dashBoardVisible();
     }
 
+    @Когда("пользователь переводит {int} рублей с карты с номером {string} на свою {int} карту с главной страницы с data-test-id= {string}")
+    public void transferFromCard2ToCard1(int transferSum, String arg1, int arg2, String arg3) {
+        dashboardPage.transferFromCard2ToCard1((transferSum));
+    }
+
+    @Тогда("баланс его {int} карты из списка на главной страницы с data-test-id= {string} должен стать {int}")
+    public void checkNewBalance(int arg0, String idCard, int newBalance) {
+        assert dashboardPage.getCardBalance(idCard) == newBalance;
+    }
 }
